6c14d6c4acbb3c39f168179107d4a7a02519ac4f
YBCS <budha627@gmail.com>
2021-07-24 03:52:23 +0530

    create env, bind search, load image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from "axios"
 import Header from './components/Header/Header'
 import SearchPhoto from './components/SearchPhoto/SearchPhoto';
+import './App.css';
 
 function App() {
+  const [flicks, setFlicks] = useState([]) 
+
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_URL).then((response) => {
+      setFlicks(response.data.photos.photo);
+    });
+  }, [])
+
   return (
     <div className="App">
+      {/* let the search be inside Header component */}
+      {/* let the image be rendered from inside the SearcPhoto component */}
       <Header text='ReactJS Assignment'/>
       <SearchPhoto />
+      <div>
+        {flicks.map((flick, i) => (
+          <img 
+          src={flick.url_n} 
+          alt={flick.title} 
+          key={flick.id}
+          className='Image' 
+          />
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/SearchPhoto/SearchPhoto.js
+++ src/components/SearchPhoto/SearchPhoto.js
@@ -1,11 +1,30 @@
 import './SearchPhoto.css'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const SearchPhoto = () => {
+    const [search, setSearch] = useState('')
+
+    const handleCLick = (e) => {
+        console.log('search bar clicked!', e)
+        // implements past suggestions here
+        // store value in browser
+    }
+
+    const handleSearchChange = (event) => {
+        setSearch(event.target.value)
+        // pass this to the api
+        // bind it to a value
+    }
+
     return (
       <div className='Src'>
         <h3 id='word'>Search Photos</h3>
-        <input placeholder="keyword here"></input>
+        <input 
+        placeholder='keyword here'
+        value={search}
+        onClick={handleCLick}
+        onChange={handleSearchChange}
+        />
       </div>
       // add suggestions in dropdown
     )
