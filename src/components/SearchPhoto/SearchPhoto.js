2d8076b8a5e191360aa05efda05d85f77dde4c63
YBCS <budha627@gmail.com>
2021-07-23 23:31:05 +0530

    basic header

---
+++ src/components/SearchPhoto/SearchPhoto.js
@@ -0,0 +1,14 @@
+import './SearchPhoto.css'
+import React from 'react'
+
+const SearchPhoto = () => {
+    return (
+      <div className='Src'>
+        <h3 id='word'>Search Photos</h3>
+        <input placeholder="keyword here"></input>
+      </div>
+      // add suggestions in dropdown
+    )
+  }
+
+export default SearchPhoto
